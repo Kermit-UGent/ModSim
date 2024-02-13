7237d0c28d5981917c9f42d3ae4ec4f19181da54
michielstock <michielfmstock@gmail.com>
2024-02-13 10:32:01 +0100

    :tada: add course

---
+++ make.jl
@@ -0,0 +1,17 @@
+#=
+Created on 12/02/2024 12:04:58
+Last update: -
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Builds this entire project
+=#
+
+## Run all the scripts
+
+#inlude("scrips/...")
+
+## Build Tex file of the course
+
+run(`pdflatex course/main.tex`)
