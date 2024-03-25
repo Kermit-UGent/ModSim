3437a40e62972dfad62a733199da1c6fb7aed261
michielstock <michielfmstock@gmail.com>
2024-03-25 14:35:40 +0100

    :art: script to generate figures

---
+++ makefigs.jl
@@ -0,0 +1,17 @@
+# A julia script to run the notebooks and save the figures
+
+using Plots
+
+notebooks = Dict(
+    "probmod" => "modelling_distributions.jl"
+)
+
+
+
+for (dir, nb) in notebooks
+    # run notebook and generate a plots dict
+    include(joinpath("scripts", nb))
+    for (name, pl) in plots
+        savefig(pl, joinpath("figures", dir, name*".pdf"))
+    end
+end
