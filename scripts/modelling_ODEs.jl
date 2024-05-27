14cee6910a6cf3b895d92464fc629166aa6fa900
michielstock <michielfmstock@gmail.com>
2024-05-27 09:58:38 +0200

    :wrench: working on models

--- scripts/modelling_ODEs.jl
+++ scripts/modelling_ODEs.jl
@@ -32,6 +32,12 @@ using Catalyst, DifferentialEquations
 # ╔═╡ 0686fc66-5428-451f-aa72-c0250ad4bf67
 using Symbolics
 
+# ╔═╡ f61f9ed2-8592-466c-93d0-e2ae59ed1e2e
+using ModelingToolkit
+
+# ╔═╡ 7f3a81ae-a285-4fa7-b034-cac9201115bc
+using LinearAlgebra
+
 # ╔═╡ dfca2f9f-0134-461c-a18b-f66f2bf02943
 md"# Modelling with ordinary differential equations"
 
@@ -81,13 +87,40 @@ A = 1^2 * π
 r = 0.2
 
 # ╔═╡ 73eb7d0a-5433-4e3d-a008-748db66b8ef9
-md"### Coffee example"
+md"""
+### Coffee example
+
+A cup of coffee of 0.15 L has an initial temperature of 80 degrees Celsius. The drop in temperature is determined by Newton's law of cooling, which states that the temperature change is proportional to the temperature difference in the environment (with $k$ as the proportionality parameter). Assume 20 degrees Celsius for room temperature ($T_e$). After 2 minutes, we slowly pour 5 cl milk at a temperature of 5 degrees ($T_m$) in the cooling coffee. Pouring in the milk while stirring takes 1 minute. Describe the temperature of the coffee. You can assume the heat capacity and density for coffee and milk are constant and the same.
+
+Here, we need to keep track of two states: the volume of coffee $V(t)$ and the temperature of the coffee $T(t)$. Both will be needed to describe this stimulating system: we will obtain a system of differential equations! The change in volume is rather simple:
+
+$$V'(t)=q(t)$$
+
+with 
+
+$$q(t)=\begin{cases} 
+0.05\text{ L/min} & \text{if } 2 \le t < 3 \\
+0 & \text{otherwise} 
+\end{cases}$$
+
+
+For the temperature, we have to resort to an energy balance:
+
+$$\mathrm{d}(C_p\rho V(t)T(t)) = q(t)\rho C_p T_m\mathrm{d}t + \rho C_p kV(t) (T_e - T(t))\mathrm{d}t\,,$$
+
+where the density $\rho$ (kg/L) and the specific heat capacity (J/(K $\cdot$kg)). As these are constants appearing in both terms, they can be eliminated. We see that the change in total heat in the system (relative to an arbitrary reference temperature) comprises 1) adding the (cooler) milk and 2) the passive cooling. Expanding the left hand side and dividing by $\mathrm{d}t$ gives:
+
+$$V(t)\frac{\mathrm{d}T(t)}{\mathrm{d}t} + T(t)\frac{\mathrm{d}V(t)}{\mathrm{d}t} = q(t)T_m + kV(t) (T_e - T(t))$$
+
+So, the system can be described by the following set of coupled, nonlinear differential equations:
+
+"""
 
 # ╔═╡ d3b84441-ed9f-436d-a690-660c5f4b8fbd
 function coffee!(du, u, (q, Tmilk, Tenv, k), t)
 	V, T = u
 	du[1] = dV = q(t)
-	du[2] = dT = q(t) * Tmilk - dV * T - (T - Tenv) * k 
+	du[2] = dT = q(t) * (Tmilk - T) / V  + (Tenv - T) * k 
 	return du
 end
 
@@ -97,8 +130,11 @@ end
 # ╔═╡ 10a26b97-8b0a-454e-afbf-141aef4aa04f
 qin = t -> 2 ≤ t < 3 ? 5e-2 : 0.0
 
+# ╔═╡ 0d03953b-6c00-4ffd-abdf-e0540480eb57
+@bind k Slider(0:0.1:2, default=.1)
+
 # ╔═╡ 206dbd4b-2ec0-4591-b7f9-d8e78d568c2f
-coffee_prob = ODEProblem(coffee!, [1.5e-1, 80], (0.0, 8.0), (qin, 5, 20, 0.1
+coffee_prob = ODEProblem(coffee!, [1.5e-1, 80], (0.0, 8.0), (qin, 5, 20, k
 ))
 
 # ╔═╡ bf062835-538d-437a-bae6-6309c66ebd19
@@ -108,6 +144,40 @@ coffee_sol = solve(coffee_prob, saveat=0.1, tstops=1.9:0.001:3.3)
 begin
 	plot(qin, 0:0.01:8, ls=:dash, label="q", color=:red, lw=2, xlab="t", title="Debit of milk added")
 	vspan!([2, 3], alpha=0.4, color=:pink, label="")
+end
+
+# ╔═╡ d036c1da-2fb7-42b4-81ba-97d40ee2bf8a
+md"""
+### Tanks in series
+
+Two tanks, with volumes $V_1$ and $V_2$, are connected in series: what exits in the first tank enters the second tank. Water enters the first tank at a constant flow $q$ but with a variable concentration $c_{in}(t)$ of dissolved organic matter. This first tank has the same outflow $q$ into the second tank from the bulk of the first tank. Both are well-mixed, meaning that we can assume that the concentration of both tanks is uniformly the same. 
+
+In addition, the concentration of the organic matter in the tanks degrades at a rate proportional to their concentration. For the first tank, this rate is proportional to $r_1$ and $r_2$ for the second tank. Give the ODEs that describe the concentrations $C_1(t)$ and $C_2(t)$ of both tanks.
+
+We can solve this by making a balance of the total amount of organic matter in both tanks. For the first tank, this is
+
+$$(V_1C_1(t))'  = qc_{in}(t) - q C_1(t) - r_1 C_1(t)$$
+
+and the second tank
+
+$$(V_2C_2(t))'  = q C_1(t) - q C_2(t) - r_2 C_1(t)$$
+
+Note that, because the volumes are constant, we can extract it from the derivative. A bit of algebra allows us to rewrite this as a linear system of ODEs:
+
+$$\begin{bmatrix}C_1'(t)\\C_2'(t)\end{bmatrix} = \begin{bmatrix}-(q+r_1)/V_1 & 0\\q/V_2 & -(q+r_2)/V_2\end{bmatrix} + \begin{bmatrix}qc_{in}(t)/V_1\\0\end{bmatrix}$$
+
+Suppose that 
+
+$$c_{in}(t) = 5\times(1-\cos(2\pi t/24)) \text{ g/L}$$
+
+"""
+
+# ╔═╡ 6578e920-8bd8-4adb-aa45-7f601fce20e4
+function tanks!(du, u, (V1, V2, r1, r2, q, cin), t)
+	C1, C2 = u
+	du[1] = dV = - (q + r1) / V1 * C1 + q * cin(t) / V1
+	du[2] = dT = - (q + r2) / V2 * C2 + q * C1 / V2
+	return du
 end
 
 # ╔═╡ 071b8f85-75cc-487b-a13f-64428bab7147
@@ -226,7 +296,7 @@ plot(solve(ODEProblem(growth3, [], (0., 20.))), lw=2, title="Logistic growth")
 lotka_volterra = @reaction_network begin
 	α, x --> 2x  # reproduction prey
 	β, y --> 0  # mortality pred
-	γ, x + y --> 1.1y  # predation
+	γ, x + y --> y + δ * y  # predation
 end
 
 # ╔═╡ dfb27358-b5ab-443f-8f61-ff4bafc25b02
@@ -291,18 +361,21 @@ md"### Hill function"
 # ╔═╡ fe75d100-73b1-4f80-a2cb-bea922521365
 @bind n Slider(1:10, default=5)
 
+# ╔═╡ 44be9a88-530d-47d8-bf22-543bcc63b6c5
+md"### Repressor"
+
 # ╔═╡ b3217136-69b5-42f2-a4b5-f16371fcf827
 # time in minutes
 repressor = @reaction_network begin
 	@species R(t)=0 mRNA(t)=0
-	hillr(R, vtranscr, Ki, 4), 0 --> mRNA  # transcription
-	d, mRNA --> 0  # degradation or mRNA
+	hillr(R, vtranscr, Ki, 4) , 0 --> mRNA  # transcription
 	vtransl, mRNA --> mRNA + R  # translation
+	d, mRNA --> 0  # degradation or mRNA
 	r, R --> 0  # degradation of R
 end
 
 # ╔═╡ ef74adcd-c1c2-49ba-b424-8d35881db4f7
-convert(ODESystem, repressor)
+convert(ODESystem, repressor) 
 
 # ╔═╡ 19b752fa-aad6-4ee5-a28c-29f36e8496c9
 let
@@ -324,6 +397,15 @@ end
 # ╔═╡ 1955523c-7d60-4e7a-845b-8977dc9b01fb
 convert(ODESystem, logistic)
 
+# ╔═╡ 035391fc-09eb-467c-b4cc-27bec87a8120
+two_species_competition = @reaction_network begin
+	@species A(t)=1.0 B(t)=5.0
+	@parameters ra=0.2 rb=0.15 K=100 m=0.1
+	ra*(1-(A+B)/K), A --> 2A
+	rb*(1-(A+B)/K), B --> 2B
+	m, (A, B) --> 0
+end
+
 # ╔═╡ 5140228d-a479-4232-8303-6f5b0da339d8
 md"## Compartmental models"
 
@@ -336,6 +418,9 @@ pharkin = @reaction_network begin
 	k₁, G --> B
 	k₂, B --> T
 end
+
+# ╔═╡ 4888025a-986f-4d68-88fb-24872d08a465
+convert(ODESystem, pharkin) 
 
 # ╔═╡ ff620b5c-8aa8-4b48-83bd-de533c53f52e
 md"### Reactor with dead zone"
@@ -375,19 +460,25 @@ sir = @reaction_network begin
 	γ, I --> R
 end
 
+# ╔═╡ 156385c2-09f6-48cb-bd2d-e3f7c53fac6f
+convert(ODESystem, sir) |> latexify |> clipboard
+
 # ╔═╡ e7c84c65-7f49-410e-99d8-b9345b4559d7
 md"### Leslie matrix model"
 
 # ╔═╡ f4acd112-3341-4df2-b7f3-4738bf8b3bb3
 butterfly = @reaction_network begin
 	@species E(t)=10 C(t)=0 P(t)=0 B(t)=0
-	@parameters f=0.5 s=0.05 m=0.12
+	@parameters f=1.5 s=0.05 m=0.05
 	s, E --> C
 	s, C --> P
 	s, P --> B
 	m, (E, C, P, B) --> 0
 	f, B --> B + E
 end
+
+# ╔═╡ 185c1d67-6b68-4f97-bf87-bc75bcc05894
+convert(ODESystem, butterfly) |> latexify |> clipboard
 
 # ╔═╡ 79b13806-4d55-4524-9a43-09e3dc6603a5
 # oefening: draagkracht voor 100 rupsen
@@ -397,10 +488,28 @@ convert(ODESystem, butterfly)
 
 # ╔═╡ 92d35635-cc1c-4485-8a40-9ca16a3dbee3
 let
-	pars = [:f=>5, :s=>0.05, :m=>0.12]
+	pars = [] #[:f=>5, :s=>0.05, :m=>0.12]
 	prob_sir = ODEProblem(butterfly, [], (0.0, 50.0), pars)
 	plot(solve(prob_sir), lw=2)
 end
+
+# ╔═╡ 380394bf-2c01-4efd-9d8e-6e5b0fe01dfc
+netstoichmat(butterfly)
+
+# ╔═╡ ba74ef61-1eda-4494-af29-3979ad2c63ca
+matse, _ = ModelingToolkit.linearize(convert(ODESystem, butterfly), [], species(butterfly))
+
+# ╔═╡ 9864851c-50ff-40b5-bae0-e6b2716cc868
+eigen(matse[:A])
+
+# ╔═╡ 284a1a32-0d39-4922-8b9e-f0cf882f1eb5
+mats, simplified_sys = ModelingToolkit.linearize_symbolic(convert(ODESystem, butterfly), [], species(butterfly))
+
+# ╔═╡ bf840c64-056b-47da-b1ba-d11030e31753
+mats[:A] |> latexify |> clipboard
+
+# ╔═╡ 9e99900f-833e-4a9d-8f5c-311b19090446
+simplified_sys
 
 # ╔═╡ 380b6a1d-f5da-4ba9-b7b0-3dbca0e267b8
 md"## Appendix"
@@ -428,6 +537,34 @@ let
 	plots["coffeetemp"] = p
 end
 
+# ╔═╡ 06ba3e44-f2af-45c7-917e-3c8d0ca8ba91
+let
+	V1 = 200
+	V2 = 500
+	r1 = 1
+	r2 = 0.8
+	q = 20
+	cin = t-> 10 - 10exp(-t/10)
+	prob = ODEProblem(tanks!, [0., 0.], (0.0, 100.0), (V1, V2, r1, r2, q, cin))
+	sol = solve(prob, saveat=0.1)
+	plot(cin, 0, 100, label=L"C_{in}(t)", lw=2, title="Tanks in series:\nexponential input")
+	plots["tanks_exp"] = plot!(sol, ls=:auto, label=[L"C_1(t)" L"C_2(t)"], lw=2)
+end
+
+# ╔═╡ 14be39e9-4f92-43e9-8a9a-6a38a545060b
+let
+	V1 = 200
+	V2 = 500
+	r1 = 1
+	r2 = 0.8
+	q = 20
+	cin = t -> 5 * (1 - cos(2π * t / 24))
+	prob = ODEProblem(tanks!, [0., 0.], (0.0, 100.0), (V1, V2, r1, r2, q, cin))
+	sol = solve(prob, saveat=0.1)
+	plot(cin, 0, 100, label=L"C_{in}(t)", lw=2, title="Tanks in series:\nsinusoidal input")
+	plots["tanks_cos"] = plot!(sol, ls=:auto, label=[L"C_1(t)" L"C_2(t)"], lw=2)
+end
+
 # ╔═╡ 92e9cb14-2db1-423d-ab97-e99c726844f7
 plots["LMA_50_50"] = lma_illustration(50, 50)
 
@@ -452,7 +589,7 @@ plots["log_growth"] = plot(solve(ODEProblem(growth2, [], (0., 20.))), lw=2, titl
 # ╔═╡ d5e019dc-c8b4-4296-a3c0-921f7dc4a388
 let
 	lvp = ODEProblem(lotka_volterra, [:x=>1.0, :y=>1.0], (0., 50.), 
-			[:α=>1.2, :β=>0.5, :γ=>0.5], combinatoric_ratelaws=false)
+			[:α=>1.2, :β=>0.5, :γ=>0.5, :δ=>.1], combinatoric_ratelaws=false)
 	plots["LK"] = plot(solve(lvp), lw=2, label=["prey" "predator"], title="Lotka-Volterra model")
 end
 
@@ -540,6 +677,18 @@ let
 	plots["logistic_sol"] = plot(solve(prob), lw=2, title="The logistic equation")
 end
 
+# ╔═╡ 1d5f8eb6-edda-4fff-bee0-8b35b3ccdb85
+let
+	A0, B0 = 1, 5
+	plots["comp_A(0)=$(A0)_B(0)=$(B0)"] = plot(solve(ODEProblem(two_species_competition, [:A=>A0, :B=>B0], (0., 100.))), lw=2, title="Two-species competition\n A(0)=$A0; B(0)=$B0", ls=:auto)
+end
+
+# ╔═╡ 2f0d5dd8-c724-48da-b61e-1034b7a72781
+let
+	A0, B0 = 1, 1
+	plots["comp_A(0)=$(A0)_B(0)=$(B0)"] = plot(solve(ODEProblem(two_species_competition, [:A=>A0, :B=>B0], (0., 100.))), lw=2, title="Two-species competition\n A(0)=$A0; B(0)=$B0", ls=:auto)
+end
+
 # ╔═╡ fdcfd565-349e-4047-842e-32d2dd8329e6
 let
 	for f in [0.1, 0.33, 0.66]
@@ -555,21 +704,21 @@ end
 let
 	β, γ = 0.03, 0.3
 	prob_sir = ODEProblem(sir, [], (0.0, 30.0), (;β, γ))
-	plots["sir_beta=$(β)_gamma=$v"] = plot(solve(prob_sir), lw=2, title="SIR model\nβ=$β γ=$γ", ls=:auto)
+	plots["sir_beta=$(β)_gamma=$γ"] = plot(solve(prob_sir), lw=2, title="SIR model\nβ=$β and γ=$γ", ls=:auto)
 end
 
 # ╔═╡ ac6c7513-3476-4968-9d6e-eec6d09e019d
 let
 	β, γ = 0.3, 0.3
 	prob_sir = ODEProblem(sir, [], (0.0, 30.0), (;β, γ))
-	plots["sir_beta=$(β)_gamma=$v"] = plot(solve(prob_sir), lw=2, title="SIR model\nβ=$β γ=$γ", ls=:auto)
+	plots["sir_beta=$(β)_gamma=$γ"] = plot(solve(prob_sir), lw=2, title="SIR model\nβ=$β and γ=$γ", ls=:auto)
 end
 
 # ╔═╡ d408c7dc-4a57-400d-bad2-7ac02cc4fa14
 let
 	β, γ = 0.01, 0.3
 	prob_sir = ODEProblem(sir, [], (0.0, 30.0), (;β, γ))
-	plots["sir_beta=$(β)_gamma=$v"] = plot(solve(prob_sir), lw=2, title="SIR model\nβ=$β γ=$γ", ls=:auto)
+	plots["sir_beta=$(β)_gamma=$γ"] = plot(solve(prob_sir), lw=2, title="SIR model\nβ=$β and γ=$γ", ls=:auto)
 end
 
 # ╔═╡ 2f4153cb-5aab-495b-98e8-cbdd8ae99816
@@ -593,11 +742,16 @@ length(plots)
 # ╠═d3b84441-ed9f-436d-a690-660c5f4b8fbd
 # ╠═ddd43577-eb2e-4c72-b829-d7195c165ddf
 # ╠═10a26b97-8b0a-454e-afbf-141aef4aa04f
+# ╠═0d03953b-6c00-4ffd-abdf-e0540480eb57
 # ╠═206dbd4b-2ec0-4591-b7f9-d8e78d568c2f
 # ╠═bf062835-538d-437a-bae6-6309c66ebd19
 # ╠═34906c24-e13c-422f-b330-bb905c356276
 # ╠═f3737612-5458-4c6e-a634-246b2cb8cb05
 # ╠═0cb4e475-676b-4d9c-9a75-ea0a0659c5b0
+# ╠═d036c1da-2fb7-42b4-81ba-97d40ee2bf8a
+# ╠═6578e920-8bd8-4adb-aa45-7f601fce20e4
+# ╠═06ba3e44-f2af-45c7-917e-3c8d0ca8ba91
+# ╠═14be39e9-4f92-43e9-8a9a-6a38a545060b
 # ╠═071b8f85-75cc-487b-a13f-64428bab7147
 # ╠═8ae850c8-ec2c-4a6c-9c37-1c1f93bb56e9
 # ╠═92e9cb14-2db1-423d-ab97-e99c726844f7
@@ -627,7 +781,7 @@ length(plots)
 # ╠═fa828b69-5b17-47cf-8461-7cc025d64501
 # ╠═d71fccaa-e8ed-4943-9659-d51d34a10bba
 # ╠═dfb27358-b5ab-443f-8f61-ff4bafc25b02
-# ╟─d5e019dc-c8b4-4296-a3c0-921f7dc4a388
+# ╠═d5e019dc-c8b4-4296-a3c0-921f7dc4a388
 # ╠═cd632750-68e4-48de-91be-1fc6c06ffe02
 # ╠═82d65028-e90a-4e91-bf67-d2f8da74134b
 # ╠═8001cf7b-ffce-48ba-923e-1e630106bf4b
@@ -654,17 +808,22 @@ length(plots)
 # ╠═3402d9fc-b636-4edd-aa42-68f87ca9c012
 # ╠═6732f82b-79e0-4e31-9b8d-3188246b692d
 # ╠═6a5ba438-a5aa-4481-b5cb-472b94dac066
+# ╠═44be9a88-530d-47d8-bf22-543bcc63b6c5
 # ╠═b3217136-69b5-42f2-a4b5-f16371fcf827
 # ╠═ef74adcd-c1c2-49ba-b424-8d35881db4f7
-# ╠═19b752fa-aad6-4ee5-a28c-29f36e8496c9
+# ╟─19b752fa-aad6-4ee5-a28c-29f36e8496c9
 # ╠═46b39ff2-7087-44ad-86b2-f630ec2195cc
 # ╠═1d0e45e5-4ef8-46a1-9a7b-f76302889716
 # ╠═4c73138a-fab1-405b-a442-007881dfd094
 # ╠═1955523c-7d60-4e7a-845b-8977dc9b01fb
 # ╠═30bbcf76-f92e-4291-aec2-1bfdd89bdc38
+# ╠═035391fc-09eb-467c-b4cc-27bec87a8120
+# ╠═1d5f8eb6-edda-4fff-bee0-8b35b3ccdb85
+# ╠═2f0d5dd8-c724-48da-b61e-1034b7a72781
 # ╠═5140228d-a479-4232-8303-6f5b0da339d8
 # ╠═350eb38a-4345-460f-89ca-63982ceadcb1
 # ╠═43140694-0a7d-4fe6-af17-54662c856073
+# ╠═4888025a-986f-4d68-88fb-24872d08a465
 # ╠═ff620b5c-8aa8-4b48-83bd-de533c53f52e
 # ╠═17689a8f-6da2-470a-b015-c55098c8a723
 # ╠═2f2398ed-355f-49ff-b5e0-1fa8215b51f6
@@ -673,14 +832,24 @@ length(plots)
 # ╠═fdcfd565-349e-4047-842e-32d2dd8329e6
 # ╠═2f829ebd-9a8c-41d0-854c-a8407d2b161f
 # ╠═67422772-6ffe-499a-ba86-4cf70fb2fd53
+# ╠═156385c2-09f6-48cb-bd2d-e3f7c53fac6f
 # ╠═0e2b9af8-9701-4d1c-838d-c289669522d1
 # ╠═ac6c7513-3476-4968-9d6e-eec6d09e019d
 # ╠═d408c7dc-4a57-400d-bad2-7ac02cc4fa14
 # ╠═e7c84c65-7f49-410e-99d8-b9345b4559d7
 # ╠═f4acd112-3341-4df2-b7f3-4738bf8b3bb3
+# ╠═185c1d67-6b68-4f97-bf87-bc75bcc05894
 # ╠═79b13806-4d55-4524-9a43-09e3dc6603a5
 # ╠═e0203b11-b75c-4e79-87d0-c74105894aa8
 # ╠═92d35635-cc1c-4485-8a40-9ca16a3dbee3
+# ╠═380394bf-2c01-4efd-9d8e-6e5b0fe01dfc
+# ╠═f61f9ed2-8592-466c-93d0-e2ae59ed1e2e
+# ╠═ba74ef61-1eda-4494-af29-3979ad2c63ca
+# ╠═7f3a81ae-a285-4fa7-b034-cac9201115bc
+# ╠═9864851c-50ff-40b5-bae0-e6b2716cc868
+# ╠═284a1a32-0d39-4922-8b9e-f0cf882f1eb5
+# ╠═bf840c64-056b-47da-b1ba-d11030e31753
+# ╠═9e99900f-833e-4a9d-8f5c-311b19090446
 # ╟─380b6a1d-f5da-4ba9-b7b0-3dbca0e267b8
 # ╠═07afed5f-6306-4620-98dc-ec729750850b
 # ╠═c7ee808d-3ec0-4130-b6d1-1fb993178f41
