32ccfe83fca97eab40c32152a2382eb7555f3fff
bspanoghe <bram.spanoghe@gmail.com>
2026-02-05 12:00:19 +0100

    re-restructure exercises dir structure
    project.toml needs to be in parent dir of solved exercises

---
+++ exercises/student_notebooks/P1_mtk/solved/ode_model_XTRA_tank_T_h_mtk_sol.jl
@@ -0,0 +1,348 @@
+### A Pluto.jl notebook ###
+# v0.20.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a6fbee1a-9863-11f0-1dfe-cf500206e163
+using Pkg; Pkg.activate("..")
+
+# ╔═╡ 21c2d9ad-9411-4e05-be3e-7f37deebc70d
+using Plots, PlutoUI; TableOfContents()
+
+# ╔═╡ eaa957df-d305-48fb-b2c2-4b4eb29a08be
+using DifferentialEquations, ModelingToolkit
+
+# ╔═╡ 5f3f3184-7bf8-4f40-80ca-bb48ad85e2f9
+using ModelingToolkit: t_nounits as t, D_nounits as D
+
+# ╔═╡ 0d12503e-5480-4ed8-9b4a-569b7801f855
+md"""
+# Exercise: Cylindrical tank with cooling jacket
+"""
+
+# ╔═╡ fdf5f147-5e6d-4730-8f16-ddc62ff9fcfe
+solution(text) = Markdown.MD(Markdown.Admonition("hint", "Solution", [text]));
+
+# ╔═╡ 82207f70-ca9a-409b-952d-520f3b8babc3
+md"""
+## Deriving the model equations
+"""
+
+# ╔═╡ defe58ce-d407-4ab4-a4e4-e37fc061149c
+md"""
+A cylindrical tank, surrounded by a cooling jacket, contains an initial amount of water (initial temperature $T_c$ and initial height $h_0$). The cooling jacket remains at a fixed temperature $T_c$. At a certain moment ($t=0$), a stream of warmer water with flow rate $Q_{in}$ and temperature $T_{in}$ enters the tank, and at the same moment a valve at the bottom of the tank is opened. The water in the tank is well mixed at all times. We are interested in the evolution of the temperature $T$ of the water in the tank and its height $h$.
+
+We denote $\rho$ as the mass density of the water, $c_p$ as the specific heat capacity of the water, $U_c$ the heat transfer coefficient, $V (= h \cdot A)$ the volume of the water in the tank and $Q_{out} (= \sqrt{h}/R)$ the outgoing flow at the bottom of the tank. The total amount of heat-energy of the water in the tank is $H= c_p \rho V T$ [in $J$].
+
+The following quantities are important for setting-up the model equations:
+-  $c_p \rho Q_{in} T_{in}$ : rate of heat flow due to the incoming water [$Js^{-1}$]
+-  $c_p \rho Q_{out} T$ : rate of heat flow due to the outgoing water [$Js^{-1}$]
+-  $U_c (A + \pi d h) (T - T_c)$ : rate of heat transfer due to the cooling jacket [$Js^{-1}$]
+
+For the last quantity, we have assumed that the heat transfer only occurs at the side-walls and bottom where the water is in direct contact with the tank. Hence, no heat transfer at the water surface inside the tank.
+"""
+
+# ╔═╡ 1992f00e-c2a6-479b-a81b-d272eeb2dbed
+md"""
+!!! task
+	Derive the model equations for the total heat-energy $H$ and the volume $V$ of the water inside the tank.
+"""
+
+# ╔═╡ 10c49e36-f058-4803-8dd5-a3ea2c22602d
+md"""
+!!! hint
+	- Use the 'important' quantities mentioned above.
+	- Set up:
+	``` math
+	\begin{align}
+	H &= c_p \rho V T  \\
+	V &= h A \\
+	\frac{d H}{dt} &= \dots \\
+	\frac{d V}{dt} &= \dots
+	\end{align}
+	```
+	- Think well before putting a $+$ or $-$ sign before the terms while setting-up the differential equations!
+"""
+
+# ╔═╡ 27539311-aaf7-4b94-87cf-dfc4f4d2bcc6
+solution(md"""
+``` math
+\begin{align}
+	H &= c_p \rho V T  \\
+	V &= h A \\
+	\frac{d H}{dt} &= c_p \rho Q_{in} T_{in} - c_p \rho \cfrac{\sqrt{h}}{R} T - U_c (A + \pi d h) (T - T_c)\\
+	\cfrac{dV}{dt} &= Q_{in} - \cfrac{\sqrt{h}}{R}
+\end{align}
+```
+""")
+
+# ╔═╡ 50bbe739-85fb-4403-b49a-2c210ab95701
+md"""
+## Setting up the equations
+"""
+
+# ╔═╡ de867d6d-b327-4e6f-a03d-9fb8f45dab7e
+md"""
+Define variables for the temperature $T$, the height $h$, the total heat-energy $H$ and the volume $V$ of the water in the tank. Use the following variable names: `T`, `h`, `H` and `V`. Mention the dependency on the time $t$.
+"""
+
+# ╔═╡ 3efd0cbd-293e-421d-b469-802486709698
+# @variables missing
+@variables T(t) h(t) H(t) V(t)
+
+# ╔═╡ bf1b6e89-5c90-4327-9a26-066feb2a51e4
+md"""
+Define the parameters for this model and assign their corresponding values.
+
+| Parameter | Value | Unit | Meaning |
+|:---------- |:---------- |:------------|:------------|
+| $A$    | 0.283 | $m^2$  |    cross sectional area   |
+| $d$    | 0.6 | $m$  |    diameter of the tank   |
+| $\rho$    | 1000.0 | $kg/m^3$  |  water density  |
+| $Q_{in}$    | 0.01 | $m^3/s$  |   inlet flow |
+| $T_{in}$    | 30.0 | $^{\circ}C$  |   temperature of the incoming water stream |
+| $U_c$    | 4000 | $J/(s m^2\,^{\circ}C)$  |   heat transfer coefficient |
+| $T_c$    | 15.0 | $^{\circ}C$  |   temperature of the cooling water |
+| $c_p$    | 4200 | $J/(kg ^{\circ}C)$  |   heat capacity |
+| $R$    | 200 | $s/m^{5/2}$  |   res. coeff. orifice |
+"""
+
+# ╔═╡ cb5b651d-4e26-4136-a8e2-cadbd75234db
+# @parameters missing
+@parameters R=200 Qin=0.01 Tin=30.0 Uc=4000 Tc=15.0 ρ=1000 cₚ=4200 A=0.283 d=0.6
+
+# ╔═╡ 3f94b45e-284f-4d09-9b63-8bd11215f148
+md"""
+Set up the equation for total heat-energy, the volume, the rate of change in the total heat energy and the rate of change in volume.
+"""
+
+# ╔═╡ f8cdf897-6fca-41e6-92c9-ae2a7cc36eb8
+# eq_heat = missing
+eq_heat = H ~ cₚ*ρ*V*T
+
+# ╔═╡ 2fae0b7b-1440-4adb-9531-1652b89d6f64
+# eq_volume = missing
+eq_volume = V ~ A*h
+
+# ╔═╡ 1c5707fa-cb02-49d4-bc8a-d17bb7055ac7
+# change_heat = missing
+change_heat = D(H) ~ ρ*cₚ*Qin*Tin - ρ*cₚ*sqrt(h)/R*T - Uc*(A + π*d*h)*(T - Tc)
+
+# ╔═╡ 70e23f70-aac4-46e6-b20c-18947e0153d7
+# change_volume = missing
+change_volume = D(V) ~ Qin - sqrt(h)/R
+
+# ╔═╡ 2d03afaf-55b9-4eab-8a41-5581468d97e1
+md"""
+Bundle the equations.
+"""
+
+# ╔═╡ 6052c938-48d5-4bf4-82ac-43eb42aca0ff
+# eqns_tank = missing
+eqns_tank = [eq_heat, eq_volume, change_heat, change_volume]
+
+# ╔═╡ bc764684-0ef8-4925-b874-8479affd6d12
+md"""
+## Part 1: simple simulation
+"""
+
+# ╔═╡ 370763e7-ff6c-4713-ac52-c67df0607da0
+md"""
+### Building the ODE system
+"""
+
+# ╔═╡ 016fcba0-d53a-4dd0-abfe-22460a70b8f9
+md"""
+Build the model. Name it `sys1_tank`.
+"""
+
+# ╔═╡ 38cd75e9-4820-4e5e-a692-5d684f883c24
+# @mtkbuild missing
+@mtkbuild sys1_tank = ODESystem(eqns_tank, t)
+
+# ╔═╡ 6909277e-7c4e-4d79-aaa0-3eeff6841998
+md"""
+### Create and solve the ODE problem
+"""
+
+# ╔═╡ c1d1f8a4-8934-43ff-9b35-8c291dfe016d
+md"""
+Create the ODE problem. Take as initial temperature of the water `15.0` $^{\circ}C$ and initial height of the water `0.8` $m$. Simulate for `2400.0`$s$.
+"""
+
+# ╔═╡ 3ab78b19-7d18-42d7-9962-11da4d46f5c7
+# oprob1_tank = missing
+oprob1_tank = ODEProblem(sys1_tank, [T=>15.0, h=>0.8], (0.0, 2400.0), [Qin=>0.01])
+
+# ╔═╡ fcae1777-2c2f-4ad0-ae9c-a60ff6504de7
+md"""
+Solve the ODE problem. Use `saveat=1` and `reltol=1e-9`.
+"""
+
+# ╔═╡ bcb70e32-49ac-4d04-91cf-92f8f5d6548e
+# sol1_tank = missing
+sol1_tank = solve(oprob1_tank, saveat=1, reltol=1e-9)
+
+# ╔═╡ 9b0988e7-4546-4a98-a0fe-325b1e874f93
+md"""
+### Plotting results
+"""
+
+# ╔═╡ 9117e5c7-0617-4cec-8c29-5d362fa2eb16
+md"""
+Plot the temperature (with the option `idxs=[T]`) and the height (with the option `idxs=[h]`). Use `twinx()` as first argument in the second plot instruction to plot the height on the right y-axis.
+"""
+
+# ╔═╡ ff0cd780-ea3c-407f-a18a-9404ba208b68
+# begin
+# 	plot(missing)
+# 	plot!(missing)
+# end
+begin
+	plot(sol1_tank; idxs=[T], ylabel="Temperature [°C]", legend=:bottomleft)
+	plot!(twinx(), sol1_tank; idxs=[h],color=:red, ylabel="Height [m]", xlabel="", legend=:bottomright)
+end
+
+# ╔═╡ 3cee6515-f9b8-4435-aa1a-bf93f976a618
+md"""
+!!! question
+	Why do you think the temperature first rises, goes to a maximum and then decreases to go to an equilibrium value?
+"""
+
+# ╔═╡ b27bbc52-9131-467e-85c5-03f9b0ee2e78
+md"""
+Answers: missing
+"""
+
+# ╔═╡ 75d483ad-d983-487e-901c-40d3e530780d
+md"""
+## Part 2: effect of the resistance coefficient of the orifice
+"""
+
+# ╔═╡ afb92363-2ae7-4106-a829-d20bbf8c49da
+md"""
+In this part the resistance coefficient of the orifice will be increased to `250` at the time instant of `1200.0` $s$. In order to achieve this a discrete event will be used.
+"""
+
+# ╔═╡ 55cd8777-593d-4733-b740-ab4e0ab19724
+md"""
+### Building the ODE system
+"""
+
+# ╔═╡ b968af64-6cbb-4e01-b290-1eb68b3564f9
+md"""
+Build the new model that includes the discrete event. Name it `sys2_tank`.
+"""
+
+# ╔═╡ 09cab085-e5b4-4030-a027-d1ffe9cdbdc0
+# @mtkbuild missing
+@mtkbuild sys2_tank = ODESystem(eqns_tank, t; discrete_events=[[1200]=>[R~250]])
+
+# ╔═╡ f1965bf2-6985-453e-bdba-617245c6d705
+md"""
+### Create and solve the ODE problem
+"""
+
+# ╔═╡ f3bc477c-3044-413b-b1d5-e84f485106a3
+md"""
+Create the new ODE problem. Use the same initial values and simulation time as in the previous part.
+"""
+
+# ╔═╡ 291c2204-5026-425c-a431-2b27946cf72a
+# oprob2_tank = missing
+oprob2_tank = ODEProblem(sys2_tank, [T=>15.0, h=>0.8], (0.0, 2400.0), [])
+
+# ╔═╡ d762e7f2-c030-49a4-865a-139d648e42b1
+md"""
+Solve the ODE problem. Use `saveat=1` and `reltol=1e-9`. Don't forget to take a `deepcopy` of the ODFE problem.
+"""
+
+# ╔═╡ 50719433-3e82-4614-ad19-b1610fe23916
+# sol2_tank = missing
+sol2_tank = solve(deepcopy(oprob2_tank), saveat=1, reltol=1e-9)
+
+# ╔═╡ 671696da-42ed-4d08-8234-aeea74e29a11
+md"""
+### Plotting results
+"""
+
+# ╔═╡ e5b36ae7-0a44-4c90-bc67-8f35e51d6ea7
+md"""
+Plot the temperature and the height. Use `twinx()` as first argument in the second plot instruction to plot the height on the right y-axis.
+"""
+
+# ╔═╡ 41a61694-551d-4034-879e-67066385158c
+# begin
+# 	plot()
+# 	plot!()
+# end
+begin
+	plot(sol2_tank; idxs=[T], ylabel="Temperature [°C]", legend=:bottomleft)
+	plot!(twinx(), sol2_tank; idxs=[h],color=:red, ylabel="Height [m]", xlabel="", legend=:bottomright)
+end
+
+# ╔═╡ 7f9b877b-eab3-4df5-a5a6-55fee459f272
+md"""
+!!! question
+	Is the evolution of $T$ and $h$ after the increase of $R$ according to your exceptations? Explain it.
+"""
+
+# ╔═╡ cf9f3ddf-f1ce-4bf8-b42b-18c53ccdc6d6
+md"""
+Answer: missing
+"""
+
+# ╔═╡ Cell order:
+# ╟─0d12503e-5480-4ed8-9b4a-569b7801f855
+# ╠═a6fbee1a-9863-11f0-1dfe-cf500206e163
+# ╠═21c2d9ad-9411-4e05-be3e-7f37deebc70d
+# ╠═eaa957df-d305-48fb-b2c2-4b4eb29a08be
+# ╠═5f3f3184-7bf8-4f40-80ca-bb48ad85e2f9
+# ╟─fdf5f147-5e6d-4730-8f16-ddc62ff9fcfe
+# ╟─82207f70-ca9a-409b-952d-520f3b8babc3
+# ╟─defe58ce-d407-4ab4-a4e4-e37fc061149c
+# ╟─1992f00e-c2a6-479b-a81b-d272eeb2dbed
+# ╟─10c49e36-f058-4803-8dd5-a3ea2c22602d
+# ╟─27539311-aaf7-4b94-87cf-dfc4f4d2bcc6
+# ╟─50bbe739-85fb-4403-b49a-2c210ab95701
+# ╟─de867d6d-b327-4e6f-a03d-9fb8f45dab7e
+# ╠═3efd0cbd-293e-421d-b469-802486709698
+# ╟─bf1b6e89-5c90-4327-9a26-066feb2a51e4
+# ╠═cb5b651d-4e26-4136-a8e2-cadbd75234db
+# ╟─3f94b45e-284f-4d09-9b63-8bd11215f148
+# ╠═f8cdf897-6fca-41e6-92c9-ae2a7cc36eb8
+# ╠═2fae0b7b-1440-4adb-9531-1652b89d6f64
+# ╠═1c5707fa-cb02-49d4-bc8a-d17bb7055ac7
+# ╠═70e23f70-aac4-46e6-b20c-18947e0153d7
+# ╟─2d03afaf-55b9-4eab-8a41-5581468d97e1
+# ╠═6052c938-48d5-4bf4-82ac-43eb42aca0ff
+# ╟─bc764684-0ef8-4925-b874-8479affd6d12
+# ╟─370763e7-ff6c-4713-ac52-c67df0607da0
+# ╟─016fcba0-d53a-4dd0-abfe-22460a70b8f9
+# ╠═38cd75e9-4820-4e5e-a692-5d684f883c24
+# ╟─6909277e-7c4e-4d79-aaa0-3eeff6841998
+# ╟─c1d1f8a4-8934-43ff-9b35-8c291dfe016d
+# ╠═3ab78b19-7d18-42d7-9962-11da4d46f5c7
+# ╟─fcae1777-2c2f-4ad0-ae9c-a60ff6504de7
+# ╠═bcb70e32-49ac-4d04-91cf-92f8f5d6548e
+# ╟─9b0988e7-4546-4a98-a0fe-325b1e874f93
+# ╟─9117e5c7-0617-4cec-8c29-5d362fa2eb16
+# ╠═ff0cd780-ea3c-407f-a18a-9404ba208b68
+# ╟─3cee6515-f9b8-4435-aa1a-bf93f976a618
+# ╠═b27bbc52-9131-467e-85c5-03f9b0ee2e78
+# ╟─75d483ad-d983-487e-901c-40d3e530780d
+# ╟─afb92363-2ae7-4106-a829-d20bbf8c49da
+# ╟─55cd8777-593d-4733-b740-ab4e0ab19724
+# ╟─b968af64-6cbb-4e01-b290-1eb68b3564f9
+# ╠═09cab085-e5b4-4030-a027-d1ffe9cdbdc0
+# ╟─f1965bf2-6985-453e-bdba-617245c6d705
+# ╟─f3bc477c-3044-413b-b1d5-e84f485106a3
+# ╠═291c2204-5026-425c-a431-2b27946cf72a
+# ╟─d762e7f2-c030-49a4-865a-139d648e42b1
+# ╠═50719433-3e82-4614-ad19-b1610fe23916
+# ╟─671696da-42ed-4d08-8234-aeea74e29a11
+# ╟─e5b36ae7-0a44-4c90-bc67-8f35e51d6ea7
+# ╠═41a61694-551d-4034-879e-67066385158c
+# ╟─7f9b877b-eab3-4df5-a5a6-55fee459f272
+# ╠═cf9f3ddf-f1ce-4bf8-b42b-18c53ccdc6d6
---
+++ exercises/student_notebooks/P1_mtk/solved/ode_model_tractor_seat_mtk_sol.jl
@@ -0,0 +1,336 @@
+### A Pluto.jl notebook ###
+# v0.20.4
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
+
+# ╔═╡ 893b6948-b0be-11f0-13b3-f14c2a2208bb
+using Pkg; Pkg.activate("..")
+
+# ╔═╡ 47bc4a5d-7e5a-4278-b881-951737277dcf
+using Plots, PlutoUI; TableOfContents()
+
+# ╔═╡ 9bb68aa2-14dd-4c28-b7c1-21f05f5671e2
+using DifferentialEquations, ModelingToolkit
+
+# ╔═╡ fa5bb459-e754-401d-84a3-0ae75fb9b914
+using ModelingToolkit: t_nounits as t, D_nounits as D
+
+# ╔═╡ 00038789-08c2-464a-9941-1109ab47d811
+md"""
+# Exercise: Tractor seat
+"""
+
+# ╔═╡ f16b68e8-02ae-49b8-a077-c0100e2d3e89
+solution(text) = Markdown.MD(Markdown.Admonition("hint", "Solution", [text]));
+
+# ╔═╡ 40b40bcf-498b-46f8-8f5d-9eb24375d229
+md"""
+## Deriving the model equation
+"""
+
+# ╔═╡ 815032ea-12ad-446c-836c-d75e2724bed4
+md"""
+Find out how the vertical position $y$ of a tractor seat with mass $m$ varies when, after 1 second, a person with mass $M$ suddenly sits in the seat. The tractor seat consists of a spring and a shock absorber. The spring constant is denoted $k$ and the natural length of the spring is denoted $L$. The shock absorber has a damping effect which is proportional (proportionality factor $b$), but opposite, to the velocity of the seat when moving. Pay attention to the sense of forces! Assume that the seat was initially in a rest position $y_0$ and not moving ($v_0 = 0$).
+"""
+
+# ╔═╡ 00c93c86-f862-4999-8b54-4edeb85628a0
+md"""
+!!! task
+	Derive the model equation for the position $y$ of the seat. Use Newton's second law.
+"""
+
+# ╔═╡ 86940e79-05d5-43e5-afb1-590475774850
+md"""
+!!! hint
+	- Set up:
+	``` math
+	\begin{align}
+	m\cfrac{d^2 y}{dt^2} &= \cdots \\
+	\end{align}
+	```
+"""
+
+# ╔═╡ 5210f34f-928a-4a14-ab61-e2a9454508dd
+solution(md"""
+``` math
+\begin{align}
+	m\cfrac{d^2 y}{dt^2} &= -mg + k\left(L - y\right) - bv
+\end{align}
+```
+""")
+
+# ╔═╡ 5895728b-53cb-4717-8300-aedb1694c0cb
+md"""
+## Setting up the equations
+"""
+
+# ╔═╡ 20073595-1940-406f-b30b-57a1033e9c4f
+md"""
+Define the variable for the position $y$. Use the following variable name: `y`. Mention the dependency on the time $t$.
+"""
+
+# ╔═╡ eb2ac37e-91e7-4253-9274-ef8e3a2c666b
+# @variables missing
+@variables y(t)
+
+# ╔═╡ 16b70e7d-e9a4-4406-a830-6e591fd868e7
+md"""
+Define the parameters for this model and assign their corresponding values.
+
+| Parameter | Value | Unit | Meaning |
+|:---------- |:---------- |:------------|:------------|
+| $m$    | 23.0 | $kg$  |    mass of the seat   |
+| $g$    | 9.81 | $m/s^2$  |   gravitational constant |
+| $k$    | 22570 | $N/m$  |  spring constant  |
+| $L$    | 0.40 | $m$  |   natural length of the spring |
+| $b$    | 900 | $Ns/m$  |   damping constant |
+
+Use the following names: `m`, `g`, `k`, `L` and `b`.
+"""
+
+# ╔═╡ 0472f2b5-394f-4670-81ec-3046c9826ee0
+# @parameters missing
+@parameters m=23.0 g=9.81 k=22570 L=0.40 b=900
+
+# ╔═╡ e68827a7-5f6e-45f7-b7fb-d0b41472b2c8
+md"""
+In order to know the initial position $y_0$ of the seat, calculate the steady state of $y$.
+
+Hints:
+- Set both $\cfrac{d^2 y}{dt^2}$ and $v = \cfrac{d y}{dt}$ to zero in the model equation and determine an expression for the steady state of $y$.
+- Once you have the expression it is preferable to calculate the actual value it a `let` ... `end` block. Everything in this block will be local scope and won't interfere with variable names defined elsewhere.
+- Round to two digits after the decimal point with the function `round(..., digits=2)`.
+"""
+
+# ╔═╡ d8ab108b-89af-4f3d-bb4d-dae83bd71aaf
+# let
+# 	missing
+# 	missing
+# end
+let
+	m=23.0; g=9.81; k=22570; L=0.40
+	round(L - m*g/k, digits=2)
+	# 0 = -m*g + k*(L - y)
+	# <=> -k*y = m*g - k*L    # is of the form Ax=b => x = A\b
+	round((-k)\(m*g-k*L), digits=2)
+end
+
+# ╔═╡ 42a05adc-a5ac-43d5-9e0b-d8cb1f539c09
+md"""
+The person that will sit in the seat has a mass of `80.0` $kg$. Use a normal variable for the parameter `M`, the mass of the person.
+"""
+
+# ╔═╡ be8bff08-aa49-4ef4-9be6-d13a8aac01c5
+# missing
+M=80.0
+
+# ╔═╡ e918256e-3999-4ab2-8792-7da6ba32ab19
+md"""
+Set up the model equation.
+"""
+
+# ╔═╡ 115c48a4-4dc0-4cf0-ac34-e651e773e675
+# eq_position = missing
+eq_position = m*D(D(y)) ~ - m*g + k*(L - y) - b*D(y)
+
+# ╔═╡ 6049886b-95a3-4d9e-a4e7-d05f1e9c504a
+md"""
+## Building the ODE system
+"""
+
+# ╔═╡ 5856753b-2753-4a83-b3ec-747a7c0b9c52
+md"""
+Build the model and include the (discrete) event that when the time is `1`, `m` needs to be incremented with `M`. Name your model `sys_tractor_seat`.
+"""
+
+# ╔═╡ 75e757ef-ed89-40f7-a8d6-12884f4ccb98
+# @mtkbuild missing
+@mtkbuild sys_tractor_seat = ODESystem(eq_position, t; discrete_events = [[1]=>[m ~ m+M]])
+
+# ╔═╡ d4bc6ff0-7383-4ea2-a7e0-5719a9dfe794
+md"""
+## Create and solve the ODE problem
+"""
+
+# ╔═╡ 9270475c-1f63-4d3e-98e0-ba7d2417ee81
+md"""
+Create the ODE problem. Use the steady state value that you calculated before for the initial position $y_0$. As mentioned before $v_0 = 0$. The symbol for $\frac{dy}{dt}$ in the vector of initial conditions is `D(y)`. Use a simulation time span of `5.0` seconds. Use `[b=>b_val]` for the parameters argument so that you can see the effect of the damping coefficient on the results. The variable `b_val` is defined later and bound to a slider.
+"""
+
+# ╔═╡ a6ca51e5-4019-4aa1-a667-66af70b77f8a
+md"""
+Solve the ODE problem. Make a deepcopy of the ODE problem, use `Tsit5()` and `saveat=0.01`.
+"""
+
+# ╔═╡ 195752fc-f480-4996-98ae-41c0daf74942
+@bind b_val Slider(200:50:4000, default=900, show_value=true)
+
+# ╔═╡ db295a4e-e877-46c6-9f7d-b3b9fa8d04d4
+# oprob_treactor_seat = missing
+oprob_treactor_seat = ODEProblem(sys_tractor_seat, [y=>0.39, D(y)=>0.0], (0.0, 5.0), [b=>b_val])
+
+# ╔═╡ 7c1edab7-722d-4014-90c0-1b834e80aec7
+# sol_tractor_seat = missing
+sol_tractor_seat = solve(deepcopy(oprob_treactor_seat), Tsit5(), saveat=0.01);
+
+# ╔═╡ dbe1a845-2cdc-45c0-9966-3187e67ef296
+md"""
+## Plotting results
+"""
+
+# ╔═╡ a4dbf8e2-044a-427a-b773-9b5b6281bffc
+md"""
+Plot the position $y$ of the seat over time. Use the option `idxs=[y]` and `ylim=(0.32, 0.392)`. Play with the sliders above to see their effect.
+"""
+
+# ╔═╡ 0861aabb-60b5-442b-bb0d-a26ed10e7c00
+# missing
+plot(sol_tractor_seat; idxs=[y], ylim=(0.32, 0.392))
+
+# ╔═╡ 0d90986f-7c35-4145-aa50-9093725b1440
+md"""
+Check out the final value of $y$.
+"""
+
+# ╔═╡ d8db3d47-7a0d-4d59-8820-5dc81475b5e3
+# missing
+sol_tractor_seat[y][end]
+
+# ╔═╡ 37c21c15-7e1e-4dc5-9cce-eaacba2be5d4
+md"""
+!!! questions
+	1. Can you interprete the plot?
+	2. What is the approximate value of $b$ in order to have a critically damped motion? (A critically damped motion is when a system returns to its equilibrium position as quickly as possible without oscillating or overshooting).
+	3. Does the steady state value depend on the value of $b$?
+"""
+
+# ╔═╡ b7b5a8d9-5557-4fad-823c-1fb306894605
+md"""
+Answers:
+1. missing
+2. missing
+3. missing
+"""
+
+# ╔═╡ 12e1b09d-7ec4-4efe-9a27-3373a8e22116
+md"""
+Plot the velocity $v$ of the seat over time. Use `idxs=[D(y)]`.
+"""
+
+# ╔═╡ c4e74bfe-c266-4040-81b4-f5d60737d406
+# missing
+plot(sol_tractor_seat; idxs=[D(y)])
+
+# ╔═╡ 54196cf6-8a5c-4db6-ab16-209c447316b0
+md"""
+!!! question
+	If you use a considerable value for $b$, will the seat still be moving at the end of the time span?
+"""
+
+# ╔═╡ 24aeaf8b-fc8a-45d2-9972-f4866aa6d17c
+md"""
+Answer: missing
+"""
+
+# ╔═╡ b80c35e7-437e-4377-aee3-31294c680f30
+md"""
+## Calculating the steady state values
+"""
+
+# ╔═╡ 571afefb-1655-40e9-bfd2-d875db112fbe
+md"""
+Create a steady state problem and solve it. Hint: set the mass `m` to the correct value!
+"""
+
+# ╔═╡ 5a07d780-0526-482b-a445-fd21c86a3104
+# stst_val = missing
+stst_val = solve(SteadyStateProblem(sys_tractor_seat, [y=>0.35, D(y)=> 0.0], [m=>23.0+M]))
+
+# ╔═╡ 8e9e6d22-641c-41fc-aef6-4aabbccbf573
+md"""
+Show the steady state value for `y` and `D(y)`.
+"""
+
+# ╔═╡ 8b0c836a-5bb3-4490-8a22-b7ed2fa97aaa
+# missing
+stst_val[y]
+
+# ╔═╡ 6d050df4-eae3-4aef-a730-ef343cee812d
+# missing
+stst_val[D(y)]
+
+# ╔═╡ edd73175-b75f-4538-b189-8049abfe5abb
+md"""
+!!! question
+	Do the steady state values correspond to the ones you can derive from the plots?
+"""
+
+# ╔═╡ b2e0a8ee-fddc-4c8f-af07-92fb5ec9aa34
+md"""
+Answer: missing
+"""
+
+# ╔═╡ Cell order:
+# ╟─00038789-08c2-464a-9941-1109ab47d811
+# ╠═893b6948-b0be-11f0-13b3-f14c2a2208bb
+# ╠═47bc4a5d-7e5a-4278-b881-951737277dcf
+# ╠═9bb68aa2-14dd-4c28-b7c1-21f05f5671e2
+# ╠═fa5bb459-e754-401d-84a3-0ae75fb9b914
+# ╠═f16b68e8-02ae-49b8-a077-c0100e2d3e89
+# ╟─40b40bcf-498b-46f8-8f5d-9eb24375d229
+# ╟─815032ea-12ad-446c-836c-d75e2724bed4
+# ╟─00c93c86-f862-4999-8b54-4edeb85628a0
+# ╟─86940e79-05d5-43e5-afb1-590475774850
+# ╟─5210f34f-928a-4a14-ab61-e2a9454508dd
+# ╟─5895728b-53cb-4717-8300-aedb1694c0cb
+# ╟─20073595-1940-406f-b30b-57a1033e9c4f
+# ╠═eb2ac37e-91e7-4253-9274-ef8e3a2c666b
+# ╟─16b70e7d-e9a4-4406-a830-6e591fd868e7
+# ╠═0472f2b5-394f-4670-81ec-3046c9826ee0
+# ╟─e68827a7-5f6e-45f7-b7fb-d0b41472b2c8
+# ╠═d8ab108b-89af-4f3d-bb4d-dae83bd71aaf
+# ╟─42a05adc-a5ac-43d5-9e0b-d8cb1f539c09
+# ╠═be8bff08-aa49-4ef4-9be6-d13a8aac01c5
+# ╟─e918256e-3999-4ab2-8792-7da6ba32ab19
+# ╠═115c48a4-4dc0-4cf0-ac34-e651e773e675
+# ╟─6049886b-95a3-4d9e-a4e7-d05f1e9c504a
+# ╟─5856753b-2753-4a83-b3ec-747a7c0b9c52
+# ╠═75e757ef-ed89-40f7-a8d6-12884f4ccb98
+# ╟─d4bc6ff0-7383-4ea2-a7e0-5719a9dfe794
+# ╟─9270475c-1f63-4d3e-98e0-ba7d2417ee81
+# ╠═db295a4e-e877-46c6-9f7d-b3b9fa8d04d4
+# ╟─a6ca51e5-4019-4aa1-a667-66af70b77f8a
+# ╠═7c1edab7-722d-4014-90c0-1b834e80aec7
+# ╠═195752fc-f480-4996-98ae-41c0daf74942
+# ╟─dbe1a845-2cdc-45c0-9966-3187e67ef296
+# ╟─a4dbf8e2-044a-427a-b773-9b5b6281bffc
+# ╠═0861aabb-60b5-442b-bb0d-a26ed10e7c00
+# ╟─0d90986f-7c35-4145-aa50-9093725b1440
+# ╠═d8db3d47-7a0d-4d59-8820-5dc81475b5e3
+# ╟─37c21c15-7e1e-4dc5-9cce-eaacba2be5d4
+# ╠═b7b5a8d9-5557-4fad-823c-1fb306894605
+# ╟─12e1b09d-7ec4-4efe-9a27-3373a8e22116
+# ╠═c4e74bfe-c266-4040-81b4-f5d60737d406
+# ╟─54196cf6-8a5c-4db6-ab16-209c447316b0
+# ╠═24aeaf8b-fc8a-45d2-9972-f4866aa6d17c
+# ╟─b80c35e7-437e-4377-aee3-31294c680f30
+# ╟─571afefb-1655-40e9-bfd2-d875db112fbe
+# ╠═5a07d780-0526-482b-a445-fd21c86a3104
+# ╟─8e9e6d22-641c-41fc-aef6-4aabbccbf573
+# ╠═8b0c836a-5bb3-4490-8a22-b7ed2fa97aaa
+# ╠═6d050df4-eae3-4aef-a730-ef343cee812d
+# ╟─edd73175-b75f-4538-b189-8049abfe5abb
+# ╠═b2e0a8ee-fddc-4c8f-af07-92fb5ec9aa34
