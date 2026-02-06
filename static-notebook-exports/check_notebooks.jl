e25719a79ee0cbfcf3432fea47e06eb9ced44202
Bram Spanoghe <70662421+bspanoghe@users.noreply.github.com>
2026-02-06 15:01:00 +0100

    fix minor scoping issue

--- static-notebook-exports/check_notebooks.jl
+++ static-notebook-exports/check_notebooks.jl
@@ -1,11 +1,10 @@
-# use julia 1.10!
 using Pkg; Pkg.activate("./exercises/solved_notebooks")
 using PlutoStaticHTML
 
 cd(@__DIR__)
 
 notebookdir = "../exercises/solved_notebooks"
-any_failed = false # check if any notebook has failed compilation
+any_notebook_failed = false # check if any notebook has failed compilation
 
 subdirs = readdir(notebookdir, join = true) |> x -> filter(isdir, x)
 for subdir in subdirs
@@ -13,9 +12,9 @@ for subdir in subdirs
     try # prevent error in one practical from stopping the rest
         build_notebooks(bopts)
     catch e
-        any_failed = true
+        global any_notebook_failed = true
         @warn e.msg
     end
 end
 
-any_failed && error("One or more notebooks failed to compile.")
+any_notebook_failed && error("One or more notebooks failed to compile.")
