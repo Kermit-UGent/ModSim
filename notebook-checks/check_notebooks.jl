3f7203b22bc17651e13707fc7d6efc814b6549e5
Bram Spanoghe <70662421+bspanoghe@users.noreply.github.com>
2026-02-09 16:48:59 +0100

    Update check_notebooks.jl
    prevent notebook checking from running multiple notebooks in parallel

--- notebook-checks/check_notebooks.jl
+++ notebook-checks/check_notebooks.jl
@@ -8,7 +8,7 @@ any_notebook_failed = false # check if any notebook has failed compilation
 
 subdirs = readdir(notebookdir, join = true) |> x -> filter(isdir, x)
 for subdir in subdirs
-    bopts = BuildOptions(subdir, write_files = false)
+    bopts = BuildOptions(subdir, write_files = false, use_distributed = false)
     try # prevent error in one practical from stopping the rest
         build_notebooks(bopts)
     catch e
