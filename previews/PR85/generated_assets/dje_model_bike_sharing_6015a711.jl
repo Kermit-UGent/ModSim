3221bc21be6868fa15ef94dd8603248c8d083193
MichielStock <MichielStock@users.noreply.github.com>
2026-09-15 13:33:21 +0000

    Deploying to gh-pages from @ Kermit-UGent/ModSim@7d7bfbf55aa3d014343c2e0f6b9d25e517cdaff7 🚀

---
+++ previews/PR85/generated_assets/dje_model_bike_sharing_6015a711.jl
@@ -0,0 +1,338 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+#> [frontmatter]
+#> order = "21"
+#> title = "3. DJE_model_bike_sharing"
+#> tags = ["exercises"]
+#> layout = "layout.jlhtml"
+#> description = "Discrete jump model of a simple bike sharing system"
+#> 
+#>     [[frontmatter.author]]
+#>     name = "Gauthier Vanhaelewyn"
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    return quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
+
+# ╔═╡ 309035dd-5653-48a6-a53d-817e743279fa
+# Running this yourself? Point this at your own environment —
+# we advise one shared project in the parent folder: Pkg.activate("..")
+using Pkg; Pkg.activate("../../pluto-deployment-environment")
+
+# ╔═╡ 6b342f14-e7d5-11ef-1ea0-77ceb0d78f32
+using Markdown, InteractiveUtils
+
+# ╔═╡ 71140c81-af29-4857-8020-4f94c8bd64b3
+using Catalyst, JumpProcesses, StatsPlots, StatsBase
+
+# ╔═╡ 284f5847-9c15-41f3-a595-1e12a22df69f
+using PlutoUI; TableOfContents()
+
+# ╔═╡ 1f975552-b0b8-4830-8dcc-214574d4fc38
+md"""
+# Exercise: Modeling a simple Bike Sharing System
+"""
+
+# ╔═╡ a0da1455-bf8c-45d2-893a-f9b5c279cb59
+# https://www.rete8.it/wp-content/uploads/2016/04/ciclostazione-777x437.jpg
+md"""
+![Bike sharing station](https://www.rete8.it/wp-content/uploads/2016/04/ciclostazione-777x437.jpg)
+"""
+
+# ╔═╡ d2f32eab-0b35-4794-9219-5bcbb4c069c5
+md"""
+Imagine a bike sharing system for students traveling between Olin College and Wellesley College, which are about three miles apart in eastern Massachusetts. Suppose the system contains 12 bikes and two bike racks, one at Olin and one at Wellesley, each with the capacity to hold 12 bikes. As students arrive, check out a bike, and ride to the other campus, the number of bikes in each location changes.
+Initially there are `10` bikes at Olin and, hence, `2` bikes at Wellesley. For this simple model, we will also assume that the changes in the number of bikes at both locations is instantaneous. The rate at which a bike is moved from Olin to Wellesley is denoted as $p_1$ ($\#bikes\;min^{-1}$); the rate at which a bike is moved from Wellesley to Olin is denoted as $p_2$ ($\#bikes\;min^{-1}$). Both processes are zeroth-order and we want to see the evolution of bikes during $1\,h = 60\,min$.
+
+This is a discreet and stochastic problem and you need to solve it with SSA.
+"""
+
+# ╔═╡ 016842c9-9479-4061-a27e-9dc006121f23
+md"""
+Create a *reaction network object* model for the aforementioned problem in order to simulate the evolution of the number of bikes at Olin ($O$) and Wellesley ($W$) with time. Name it `bike_sharing`.
+
+In order to make sure that $O$ does not become negative, you can use either `ifelse(O>0, 1, 0)` or `>(O, 0)` as a multiplication factor to the rate `p₁`. A similar multiplication factor must be applied to the other rate `p₂`.
+"""
+
+# ╔═╡ 6c97bf81-ef32-45a4-aa7c-c8c26ba2d2c3
+# bike_sharing = @reaction_network begin
+#     @species missing
+#     missing
+#     missing
+# end
+
+# ╔═╡ 7227a95a-ba0c-44dc-b0b8-18d6bbf362e8
+md"""
+!!! tip "Tip"
+	Subscripts 1, 2, etc, can be visualized by typing, after the letter, a **backslash** followed by an **underscore** and then the **TAB** key. For example `p\_1` followed by the TAB key will result in `p₁`.
+"""
+
+# ╔═╡ 9c7ab7fb-7380-41a3-85ea-714478ade218
+md"""
+Convert the system to a symbolic differential equation model and verify, by analyzing the differential equation, that your model is correctly implemented.
+"""
+
+# ╔═╡ 1536fe23-0f8d-4b86-98d2-076248b35954
+missing
+
+# ╔═╡ 613d7769-cc9d-471e-b3e7-4266fc0e4677
+md"""
+## Part 1: Simulation for different p₁ values
+"""
+
+# ╔═╡ 6614bdde-fed0-45aa-b7ca-a21ca72dd181
+md"""
+Simulate the evolution of $O$ and $W$ for values of `p₁` in the range `[0.0, 1.0]` with a stepsize of `0.1` using a slider.
+"""
+
+# ╔═╡ e6e2ff5c-38eb-4ba3-b430-c9031483a0a5
+md"""
+Initialize a vector `u0` with the initial conditions:
+"""
+
+# ╔═╡ ab6af765-1cde-4da8-bbc1-a5fab391db54
+u0 = missing
+
+# ╔═╡ 378878a0-5c09-4eb0-ac43-1031014ff12a
+md"""
+Set the timespan for the simulation:
+"""
+
+# ╔═╡ 3ae98e83-7beb-4597-89be-80c813d4349b
+tspan = missing #floats!
+
+# ╔═╡ 988f79c0-9c7b-4752-a7f2-d4473ad73ce6
+md"""
+Create a slider for the variable `p₁` in the range of `0.0` and `1.0` with a step of `0.1`. Take a default value of `0.0`.
+"""
+
+# ╔═╡ 0d8f53f8-0a14-4ac6-bd0c-2190d4db0909
+# @bind missing
+
+# ╔═╡ 08d43ac8-a973-4d7b-baf7-4c37e54cfe24
+md"
+Initialize vector `parms` with parameter values, `p₁` is the slider value and assign a constant value of `0.3` to `p₂`.
+"
+
+# ╔═╡ e20e4dd8-bdbb-4005-af68-6bf7e4ec130e
+parms = missing
+
+# ╔═╡ 238e1120-34af-4d57-8efa-aa80ab28a874
+md"""
+Create a DiscreteProblem and store it in `dprob`:
+"""
+
+# ╔═╡ d4c45709-70c9-4ba0-8fb8-6b600473723d
+dprob = missing
+
+# ╔═╡ d06fb076-76e4-4248-a940-96804ea68833
+md"""
+Create a JumpProblem and store it in `jdprob`. Use the simulation method `Direct()`."""
+
+# ╔═╡ 7644adf4-d992-48b1-b40a-12fdf30f6cb5
+jdprob = missing
+
+# ╔═╡ 74708270-b1ec-48c7-af32-3b970b92c706
+md"""
+Solve the problem and store it in `jdsol`.
+"""
+
+# ╔═╡ 2b00df5d-994e-47a1-8068-c93ce3f1a618
+jdsol = missing
+
+# ╔═╡ 9d06c31e-3525-4889-a1de-3fe02413c7d8
+md"""
+Plot the solution. Limit the plot to `(0, 12)` for the vertical axis.
+"""
+
+# ╔═╡ 9a90f800-3669-4831-b50b-c5405bbb9a03
+missing
+
+# ╔═╡ a554fd16-aa3d-48ca-8de6-5582725c27d8
+md"""
+Analyse the results. See what happens when you:
+- run the notebook cell with the `solve` function repeatly
+- change the value of `p₁` using the slider
+"""
+
+# ╔═╡ d6872046-b5ef-4c2d-a9bb-2418f57f715d
+md"""
+!!! question "Question"
+	From what value of `p₁` do you start to get empty bike racks at Olin?
+"""
+
+# ╔═╡ 747e20c4-b06b-4e78-a09a-55053cf42bf4
+md"- Answer: missing"
+
+# ╔═╡ ce18866b-5cb8-4966-81c7-683fa65823ff
+md"""
+## Part 2: Mean zero counts at Olin
+"""
+
+# ╔═╡ dcad848a-c178-4ce5-82d0-8a11aabb3b3c
+md"""
+We now want to have an idea of the mean zero counts at Olin for `p₁` values in the range `[0.0, 1.0]`.
+"""
+
+# ╔═╡ 92181028-60fc-4830-afba-2380ac91455d
+md"""
+You can inspect the actual number of bike values at Olin by using `jdsol[:O]`:
+"""
+
+# ╔═╡ f8942b10-773a-4b22-baad-8004fba8bd34
+missing
+
+# ╔═╡ 43d41284-053d-4dfe-8d5b-96be70c0495c
+md"""
+If you want to have a `true` boolean value on positions where the vector value is zero (and `false` on non-zero values), then you would compare `jdsol[:O]` element wise with `0`. In Julia, if you want to do element wise operations with/on vectors, you always need to place a dot (`.`) in front of the operator, like for example `.==`.
+
+Compare in that way `jdsol[:O]` with `0`:
+"""
+
+# ╔═╡ a999ae2a-7567-41e7-9c0c-e94fad6f5d46
+missing
+
+# ╔═╡ 9ebb5b44-04d7-4b89-acdb-e40a245703d2
+md"""
+Furthermore, if you want the count the number of `true` values in the latter (hence, the zero element values), you can simply use the function `count(...)`. Count the number of zeros:
+"""
+
+# ╔═╡ 049de8d5-b221-452b-b2c4-9bc1e0c17f48
+missing
+
+# ╔═╡ a73a2853-1f48-4179-9771-083794d3f137
+md"""
+Using the aforementioned way to count zeros in a vector, we will now count the zeros for a range of $p$ values. Because of the stochastic behaviour of the system, for each $p$ values we will count the zeros for a $1000$ simulations and then storing only the average value.
+
+To introduce a new value for $p_1$ you can take a deepcopy of the problem and remake the problem like this:
+- `jdprob_re = remake(deepcopy(jdprob); p=[:p₁=>p_val])`
+and then solving the problem and store it in `jdsol_re`.
+
+In the layout below, `mean_zero_counts` while contain the final mean values of the averaged numbers of zeros from a `1000` simulations using a specific $p$ value, `zero_counts_p_val` will contain the actual number of zeros for a `1000` simulations using a specific $p$ value.
+
+Use the layout below to fill in `mean_zero_counts`.
+"""
+
+# ╔═╡ b82da9cc-ccba-45c4-92c4-2c3b2c190688
+md"""
+!!! warning "Important note"
+
+	The SSA solver only saves the state when something changes (for example, when a bike arrives or leaves). It does **not** automatically store values in between events, so it does not explicitly keep track of how long the system stays in the same state.
+
+	If we want to estimate how long there were zero bikes, we need information at regular time intervals. We can do this by setting `saveat = 0.1`. This forces the solver to record the state every 0.1 time units, thereby approximating the time that there are 0 bikes at the campus. 
+
+	Because the timepoints at which a state changes are random, we will still have a small error due to the number of bikes changing in between our chosen time intervals. Choosing a small time interval will help reduce this error. 
+"""
+
+# ╔═╡ 682e9120-0e1c-4dfa-9ec6-66bb0a3f4374
+# begin
+# 	p_values = 0.0:0.1:1.0  # different p-values
+# 	mean_zero_counts = []   # vector to store the corresponding mean zero values
+# 	for p_val in p_values    # p_val will be each of the p_values
+# 		zero_counts_p_val = []  # vector to store the zeros for the 1000 simulations
+# 		for i = missing          # do a 1000 simulation
+# 			# take a deepcopy and remake the problem for the specific p-value
+# 			jdprob_re = missing
+# 			# solve the problem
+# 			jdsol_re = missing;
+# 			# append the number of zeros to zero_counts_p_val
+# 			missing
+# 		end
+# 		# append the mean number of zeros to mean_zero_counts
+# 		missing
+# 	end
+# end
+
+# ╔═╡ 705d3fcb-20b6-4481-a304-1d3ccd623674
+md"""
+Have a look at the mean zero counts by typing `mean_zero_counts`:
+"""
+
+# ╔═╡ 5968317a-6c07-4655-8137-6702656bb3b4
+missing
+
+# ╔═╡ ff9370d8-3395-4382-9f51-afa11748319e
+md"""
+Plot the mean zero counts as a function of the $p$-values.
+"""
+
+# ╔═╡ 48be49d0-0b60-44f3-8152-1ca917a4232e
+missing
+
+# ╔═╡ d6452915-bdf0-48f0-8c7d-3df83c7bce72
+md"""
+!!! question "Questions"
+	1. From what value of $p$ do the empty number of bike racks at Olin clearly begin to rise?
+	2. Reflect on this, does this make sense? Hint: change the value of $p_2$ and observe what happens.
+"""
+
+# ╔═╡ 4d73e614-9360-4e13-b7a4-ff7713989bf8
+md"""
+Answers:
+1. missing
+2. missing
+"""
+
+# ╔═╡ Cell order:
+# ╟─1f975552-b0b8-4830-8dcc-214574d4fc38
+# ╠═6b342f14-e7d5-11ef-1ea0-77ceb0d78f32
+# ╠═309035dd-5653-48a6-a53d-817e743279fa
+# ╠═71140c81-af29-4857-8020-4f94c8bd64b3
+# ╠═284f5847-9c15-41f3-a595-1e12a22df69f
+# ╟─a0da1455-bf8c-45d2-893a-f9b5c279cb59
+# ╟─d2f32eab-0b35-4794-9219-5bcbb4c069c5
+# ╟─016842c9-9479-4061-a27e-9dc006121f23
+# ╠═6c97bf81-ef32-45a4-aa7c-c8c26ba2d2c3
+# ╟─7227a95a-ba0c-44dc-b0b8-18d6bbf362e8
+# ╟─9c7ab7fb-7380-41a3-85ea-714478ade218
+# ╠═1536fe23-0f8d-4b86-98d2-076248b35954
+# ╟─613d7769-cc9d-471e-b3e7-4266fc0e4677
+# ╟─6614bdde-fed0-45aa-b7ca-a21ca72dd181
+# ╟─e6e2ff5c-38eb-4ba3-b430-c9031483a0a5
+# ╠═ab6af765-1cde-4da8-bbc1-a5fab391db54
+# ╟─378878a0-5c09-4eb0-ac43-1031014ff12a
+# ╠═3ae98e83-7beb-4597-89be-80c813d4349b
+# ╟─988f79c0-9c7b-4752-a7f2-d4473ad73ce6
+# ╠═0d8f53f8-0a14-4ac6-bd0c-2190d4db0909
+# ╟─08d43ac8-a973-4d7b-baf7-4c37e54cfe24
+# ╠═e20e4dd8-bdbb-4005-af68-6bf7e4ec130e
+# ╟─238e1120-34af-4d57-8efa-aa80ab28a874
+# ╠═d4c45709-70c9-4ba0-8fb8-6b600473723d
+# ╟─d06fb076-76e4-4248-a940-96804ea68833
+# ╠═7644adf4-d992-48b1-b40a-12fdf30f6cb5
+# ╟─74708270-b1ec-48c7-af32-3b970b92c706
+# ╠═2b00df5d-994e-47a1-8068-c93ce3f1a618
+# ╟─9d06c31e-3525-4889-a1de-3fe02413c7d8
+# ╠═9a90f800-3669-4831-b50b-c5405bbb9a03
+# ╟─a554fd16-aa3d-48ca-8de6-5582725c27d8
+# ╟─d6872046-b5ef-4c2d-a9bb-2418f57f715d
+# ╠═747e20c4-b06b-4e78-a09a-55053cf42bf4
+# ╟─ce18866b-5cb8-4966-81c7-683fa65823ff
+# ╟─dcad848a-c178-4ce5-82d0-8a11aabb3b3c
+# ╟─92181028-60fc-4830-afba-2380ac91455d
+# ╠═f8942b10-773a-4b22-baad-8004fba8bd34
+# ╟─43d41284-053d-4dfe-8d5b-96be70c0495c
+# ╠═a999ae2a-7567-41e7-9c0c-e94fad6f5d46
+# ╟─9ebb5b44-04d7-4b89-acdb-e40a245703d2
+# ╠═049de8d5-b221-452b-b2c4-9bc1e0c17f48
+# ╟─a73a2853-1f48-4179-9771-083794d3f137
+# ╟─b82da9cc-ccba-45c4-92c4-2c3b2c190688
+# ╠═682e9120-0e1c-4dfa-9ec6-66bb0a3f4374
+# ╟─705d3fcb-20b6-4481-a304-1d3ccd623674
+# ╠═5968317a-6c07-4655-8137-6702656bb3b4
+# ╟─ff9370d8-3395-4382-9f51-afa11748319e
+# ╠═48be49d0-0b60-44f3-8152-1ca917a4232e
+# ╟─d6452915-bdf0-48f0-8c7d-3df83c7bce72
+# ╠═4d73e614-9360-4e13-b7a4-ff7713989bf8
---
+++ previews/PR85/generated_assets/ode_model_birth_death_247fa92f.jl
@@ -0,0 +1,258 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+#> [frontmatter]
+#> order = "10"
+#> title = "2. ODE_model_birth_death"
+#> tags = ["exercises"]
+#> layout = "layout.jlhtml"
+#> description = "Simple birth-death model for a mice population"
+#> 
+#>     [[frontmatter.author]]
+#>     name = "Gauthier Vanhaelewyn"
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 12fee37d-eef9-4e90-b0af-d892469fad08
+# Running this yourself? Point this at your own environment —
+# we advise one shared project in the parent folder: Pkg.activate("..")
+using Pkg; Pkg.activate("../../pluto-deployment-environment")
+
+# ╔═╡ 4a3d066c-f5b1-11ee-0145-2da7c11147a5
+using Markdown, InteractiveUtils
+
+# ╔═╡ 34a2f237-848c-4d07-9cea-fa5505a9e215
+using StatsPlots, PlutoUI; TableOfContents()
+
+# ╔═╡ b721cc4d-43ba-4221-bbd1-15293aaf54b5
+using OrdinaryDiffEq, Catalyst
+
+# ╔═╡ c701d64e-640c-473f-b0fa-688024962f28
+md"""
+# Exercise: Simple birth-death model for mice
+"""
+
+# ╔═╡ 6e4f08fa-541d-4ca5-a152-6e98a4518690
+md"""
+![](https://users.ugent.be/~gvhaelew/fig/mice_model_part1.png)
+"""
+
+# ╔═╡ f1743720-bdeb-4f25-9274-f7e01674dcb1
+md"""
+In a simple birth-death model for mice, the birth rate of mice represents the
+rate at which new individuals are added to the population through reproduction.
+This rate is influenced by factors such as the number of reproductive females,
+their fertility, and the frequency of reproduction cycles. Conversely,
+the death rate reflects the rate at which individuals are removed from
+the population due to mortality factors such as predation, disease,
+and environmental stressors. Together, these rates interact dynamically
+to shape the population dynamics of mice in their natural habitat.
+Denote the number of mice by $X$, the average birth rate by $b$ ($mice / day$), and the average death rate by $d$ ($day^{-1}$). Hence, assume for this overly simplified model, that the birth of mice is a zeroth-order process and that the death of mice is a first-order process.
+"""
+
+# ╔═╡ b583efef-dc7d-4447-bf27-e6373f5872c4
+md"""
+Create a *reaction network object* model for the aforementioned problem in order to simulate the evolution of $X$ with time. Name it `birth_death`.
+"""
+
+# ╔═╡ 74c3f7ed-d705-4cb9-b52c-e06c9df5ca13
+# birth_death = @reaction_network begin
+# 	missing
+# end
+
+# ╔═╡ c84f2a32-e66b-4a36-a257-7031ce799225
+md"""
+Convert the system to a symbolic differential equation model and verify, by analyzing the differential equation, that your model is correctly implemented.
+"""
+
+# ╔═╡ 124eb20e-6945-4eec-a05a-6b835efbdd2e
+osys  = missing
+
+# ╔═╡ 04c645ad-873d-44c1-9bc0-940a412630b9
+md"""
+## Part 1
+
+Simulate the evolution of the number of mice **per day** during $10$ years starting off with $2$ mice. **Assume that per year 25 pups are born.** Suppose the death rate to be $0.0015\;day^{-1}$.
+"""
+
+# ╔═╡ 1cd5136a-f472-4329-980a-b8382d1c04ea
+md"""
+First, calculate the birth rate in $mice/day$.
+"""
+
+# ╔═╡ 6b64ec8b-d85e-4a4f-aa3f-f177f5e34180
+missing
+
+# ╔═╡ 52ee74d2-4dca-47ef-8b97-f826ef31ddf9
+md"""
+Initialize a vector `u0` with the initial conditions:
+"""
+
+# ╔═╡ 23f282d8-2eeb-43dd-8ba9-84684daca7a4
+u0 = missing
+
+# ╔═╡ 30fa6fc6-54e0-4233-9334-b3d15f630489
+md"""
+Set the timespan for the simulation:
+"""
+
+# ╔═╡ b461a225-bae6-45b8-bb42-54742a72b98f
+tspan = missing
+
+# ╔═╡ c753d65f-9ed3-4e60-8e36-6e4bca77c19b
+md"""
+Initialize a vector `parms` with the parameter values:
+"""
+
+# ╔═╡ e7942ba9-5434-4b22-b1d4-f18f6227320d
+parms = missing
+
+# ╔═╡ 2af7f6b9-9d95-4bf6-af67-449803440639
+md"""
+Create the ODE problem and store it in `oprob`:
+"""
+
+# ╔═╡ 9432f90b-7141-4518-b6fd-d55a1389e14a
+oprob = missing;
+
+# ╔═╡ ab197ac7-413b-4305-8352-2723bf9f2aff
+md"""
+Solve the ODE problem. Use `Tsit5()` and `saveat=1.0`. Store the solution in `osol`:
+"""
+
+# ╔═╡ 8e517114-7c74-4d7c-954c-2787d975a1df
+osol = missing
+
+# ╔═╡ 9eaf1ca5-3d65-4263-a307-991bc8bf62d2
+md"""
+Plot the results:
+"""
+
+# ╔═╡ 83e36e63-7e6b-4c17-8f7d-ed0e07f36fc5
+missing
+
+# ╔═╡ 5fc64787-795c-4e17-b751-cd3683f8016b
+md"""
+!!! question
+	Interpret the results. Ask yourself the following questions:
+	- What is the (approximate) steady state value for $X$?
+"""
+
+# ╔═╡ f5718c08-c8aa-4d74-b70f-8c08c6decab2
+md"- Answer: missing"
+
+# ╔═╡ fe4526c8-4504-4d91-841a-a83aeef55fc7
+md"""
+## Part 2
+
+Suppose that at $t = 3\;years$ the death rate of the mice population increases by $50\,\%$ due to a new predator species in the area. Use the same initial condition, timespan and parameter values. Simulate the evolution of the number of mice.
+"""
+
+# ╔═╡ 1e869025-c537-4bf1-9c38-93e24b598156
+md"""
+Create the *condition*. Store it in `condition2`:
+"""
+
+# ╔═╡ 1c5d8daa-5c7e-49ef-a6da-6459a1131320
+condition2 = missing
+
+# ╔═╡ 4d9ba697-5db3-4879-be88-278765e766f1
+md"""
+Make a new *reaction system* where the discrete event is included. Name it `birth_death2`.
+"""
+
+# ╔═╡ d40c2598-5783-49c5-916d-296e946de3f6
+# @named birth_death2 = missing
+
+# ╔═╡ afbc4f00-0b4a-427a-9630-3a279ccbc330
+md"""
+Complete the new *reaction system*. Name it `birth_death2_com`.
+"""
+
+# ╔═╡ 3dd3febc-fc3f-477f-a5be-335ae90223e3
+birth_death2_com = missing
+
+# ╔═╡ cf05864f-37f0-4fbb-93a5-ef406a2ae014
+md"""
+Create the ODE problem and store it in `oprob2`:
+"""
+
+# ╔═╡ 65e24cee-06c3-4cb7-8f8c-61e02b7dcd29
+oprob2 = missing;
+
+# ╔═╡ 9cab94b5-6d2d-4b5a-96f3-3b8eee1c61a1
+md"""
+Solve the ODE problem. Make a deepcopy and use `Tsit5()` and `saveat=1.0`. Store the solution in `osol2`:
+"""
+
+# ╔═╡ 00750aa4-6a00-4d33-a0ab-2d7abe952078
+osol2 = missing
+
+# ╔═╡ 694e5b50-f4c9-4ecc-8860-1b217696e1ea
+md"""
+Plot the results:
+"""
+
+# ╔═╡ c69d0e86-0d15-4915-a890-8abd378da59a
+missing
+
+# ╔═╡ 1f869579-7019-4120-8d14-b38f48d05bdd
+md"""
+!!! questions
+	Interpret the results. Ask yourself the following questions:
+	1. Can you clearly see the effect of the increase in the death rate?
+	2. If the death rate increases at a different timepoint, would you reach the same steady state value for $X$? Explain.
+"""
+
+# ╔═╡ 775f2cf3-ffbb-491a-9002-3a39b9c70f3c
+md"""
+Answers:
+1. missing
+2. missing
+"""
+
+# ╔═╡ Cell order:
+# ╟─c701d64e-640c-473f-b0fa-688024962f28
+# ╠═4a3d066c-f5b1-11ee-0145-2da7c11147a5
+# ╠═12fee37d-eef9-4e90-b0af-d892469fad08
+# ╠═34a2f237-848c-4d07-9cea-fa5505a9e215
+# ╠═b721cc4d-43ba-4221-bbd1-15293aaf54b5
+# ╟─6e4f08fa-541d-4ca5-a152-6e98a4518690
+# ╟─f1743720-bdeb-4f25-9274-f7e01674dcb1
+# ╟─b583efef-dc7d-4447-bf27-e6373f5872c4
+# ╠═74c3f7ed-d705-4cb9-b52c-e06c9df5ca13
+# ╟─c84f2a32-e66b-4a36-a257-7031ce799225
+# ╠═124eb20e-6945-4eec-a05a-6b835efbdd2e
+# ╟─04c645ad-873d-44c1-9bc0-940a412630b9
+# ╟─1cd5136a-f472-4329-980a-b8382d1c04ea
+# ╠═6b64ec8b-d85e-4a4f-aa3f-f177f5e34180
+# ╟─52ee74d2-4dca-47ef-8b97-f826ef31ddf9
+# ╠═23f282d8-2eeb-43dd-8ba9-84684daca7a4
+# ╟─30fa6fc6-54e0-4233-9334-b3d15f630489
+# ╠═b461a225-bae6-45b8-bb42-54742a72b98f
+# ╟─c753d65f-9ed3-4e60-8e36-6e4bca77c19b
+# ╠═e7942ba9-5434-4b22-b1d4-f18f6227320d
+# ╟─2af7f6b9-9d95-4bf6-af67-449803440639
+# ╠═9432f90b-7141-4518-b6fd-d55a1389e14a
+# ╟─ab197ac7-413b-4305-8352-2723bf9f2aff
+# ╠═8e517114-7c74-4d7c-954c-2787d975a1df
+# ╟─9eaf1ca5-3d65-4263-a307-991bc8bf62d2
+# ╠═83e36e63-7e6b-4c17-8f7d-ed0e07f36fc5
+# ╟─5fc64787-795c-4e17-b751-cd3683f8016b
+# ╠═f5718c08-c8aa-4d74-b70f-8c08c6decab2
+# ╟─fe4526c8-4504-4d91-841a-a83aeef55fc7
+# ╟─1e869025-c537-4bf1-9c38-93e24b598156
+# ╠═1c5d8daa-5c7e-49ef-a6da-6459a1131320
+# ╟─4d9ba697-5db3-4879-be88-278765e766f1
+# ╠═d40c2598-5783-49c5-916d-296e946de3f6
+# ╟─afbc4f00-0b4a-427a-9630-3a279ccbc330
+# ╠═3dd3febc-fc3f-477f-a5be-335ae90223e3
+# ╟─cf05864f-37f0-4fbb-93a5-ef406a2ae014
+# ╠═65e24cee-06c3-4cb7-8f8c-61e02b7dcd29
+# ╟─9cab94b5-6d2d-4b5a-96f3-3b8eee1c61a1
+# ╠═00750aa4-6a00-4d33-a0ab-2d7abe952078
+# ╟─694e5b50-f4c9-4ecc-8860-1b217696e1ea
+# ╠═c69d0e86-0d15-4915-a890-8abd378da59a
+# ╟─1f869579-7019-4120-8d14-b38f48d05bdd
+# ╠═775f2cf3-ffbb-491a-9002-3a39b9c70f3c
